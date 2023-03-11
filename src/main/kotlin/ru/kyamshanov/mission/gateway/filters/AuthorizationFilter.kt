bd2b4e8603c1d2863a83055bf22528aaef130161
KYamshanov <undframe@gmail.com>
2023-03-11 13:47:03 +0400

    Разработка от 11.03.2023 -Фикс запросов из корутин

--- src/main/kotlin/ru/kyamshanov/mission/gateway/filters/AuthorizationFilter.kt
+++ src/main/kotlin/ru/kyamshanov/mission/gateway/filters/AuthorizationFilter.kt
@@ -1,5 +1,6 @@
 package ru.kyamshanov.mission.gateway.filters
 
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import org.slf4j.LoggerFactory
 import org.springframework.cloud.gateway.filter.GatewayFilterChain
@@ -28,23 +29,25 @@ internal class AuthorizationFilter(
      *
      * @see [GlobalFilter.filter]
      */
-    override fun filter(serverWebExchange: ServerWebExchange, chain: GatewayFilterChain): Mono<Void> = runBlocking {
-        var exchange = serverWebExchange
-        val authResult =
-            authenticationFactory.createFacade(AuthenticationService.MISSION).authenticateRequest(exchange).getOrThrow()
+    override fun filter(serverWebExchange: ServerWebExchange, chain: GatewayFilterChain): Mono<Void> =
+        runBlocking(Dispatchers.IO) {
+            var exchange = serverWebExchange
+            val authResult =
+                authenticationFactory.createFacade(AuthenticationService.MISSION).authenticateRequest(exchange)
+                    .getOrThrow()
 
-        if (authResult.externalUserId != null) {
-            exchange = exchange.appendHeader(EXTERNAL_ID_HEADER, authResult.externalUserId)
-        }
-        if (authResult.userId != null) {
-            exchange = exchange.appendHeader(USER_ID_HEADER_KEY, authResult.userId)
-        }
-        if (authResult.accessId != null) {
-            exchange = exchange.appendHeader(ACCESS_ID_HEADER, authResult.accessId)
-        }
+            if (authResult.externalUserId != null) {
+                exchange = exchange.appendHeader(EXTERNAL_ID_HEADER, authResult.externalUserId)
+            }
+            if (authResult.userId != null) {
+                exchange = exchange.appendHeader(USER_ID_HEADER_KEY, authResult.userId)
+            }
+            if (authResult.accessId != null) {
+                exchange = exchange.appendHeader(ACCESS_ID_HEADER, authResult.accessId)
+            }
 
-        chain.filter(exchange)
-    }
+            chain.filter(exchange)
+        }
 
     private fun ServerWebExchange.appendHeader(key: String, value: String) = request.mutate()
         .header(key, value)
